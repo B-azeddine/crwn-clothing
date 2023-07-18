9ef63047bfc8afd8e9cedd475489f0847311fd5b
B-azeddine <blidiazeddine@gmail.com>
2023-07-18 08:06:38 +0000

    created project with react and added files for homepages

--- src/homepage.cpn.jsx
+++ src/homepage.cpn.jsx
@@ -11,33 +11,33 @@ const HomePage = () => (
         </div>
       </div>
 
-	  <div className="menu-item">
-	  <div className="content">
-		<h1 className="title">JACKETS</h1>
-		<span className="subtitle">SHOP NOW</span>
-	  </div>
-	</div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
 
-	<div className="menu-item">
-	<div className="content">
-	  <h1 className="title">SNEAKERS</h1>
-	  <span className="subtitle">SHOP NOW</span>
-	</div>
-  </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
 
-  <div className="menu-item">
-  <div className="content">
-	<h1 className="title">WOMENS</h1>
-	<span className="subtitle">SHOP NOW</span>
-  </div>
-</div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
 
-<div className="menu-item">
-<div className="content">
-  <h1 className="title">MENS</h1>
-  <span className="subtitle">SHOP NOW</span>
-</div>
-</div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
     </div>
   </div>
 );
