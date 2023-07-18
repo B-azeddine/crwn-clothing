1d3b1bbb8056d78550e5c1b1d2ff42d0d5796418
B-azeddine <blidiazeddine@gmail.com>
2023-07-18 13:41:31 +0000

    dispatching our home page into smaller components ,styling our menu-item component

--- src/components/menu-item/menu-item.cpn.jsx
+++ src/components/menu-item/menu-item.cpn.jsx
@@ -7,7 +7,7 @@ const MenuItem = ({title, imageUrl,size}) => (
 	<div  className='background-image'
 	style = {{
 		backgroundImage : `url(${imageUrl})`
-	}}/>
+	}}/> 
 	<div className="content">
 	  <h1 className="title">{title.toUpperCase()}</h1>
 	  <span className="subtitle">SHOP NOW</span>
